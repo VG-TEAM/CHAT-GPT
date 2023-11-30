c4f4fb5e3a4408c5bb864e24f36e47edee32cad7
VG-TEAM <152384469+VG-TEAM@users.noreply.github.com>
2023-11-30 05:50:43 +0530

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -161,27 +161,37 @@ async def ping(client, message: Message):
 
 #  main   
 openai.api_key = OPENAI_KEY
-@vikas.on_message(filters.command(["","",""],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+
+@vikas.on_message(filters.command(["", "", ""], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
 async def chat(bot, message):
-    
     try:
         start_time = time.time()
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+
         if len(message.command) < 2:
-            await message.reply_text(
-            "Example:**\n\n`/chatgpt Where is TajMahal?`")
+            await message.reply_text("Example:**\n\n`/chatgpt Where is TajMahal?`")
         else:
             a = message.text.split(' ', 1)[1]
             MODEL = "gpt-3.5-turbo"
-            resp = openai.ChatCompletion.create(model=MODEL,messages=[{"role": "user", "content": a}],
-    temperature=0.2)
-            x=resp['choices'][0]["message"]["content"]
-            end_time = time.time()
-            telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
-            await message.reply_text(f"{message.from_user.first_name} ᴀꜱᴋᴇᴅ:\n\n {a} \n\n {BOT_NAME} ᴀɴꜱᴡᴇʀᴇᴅ:-\n\n {x}\n\n✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ  {telegram_ping} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", parse_mode=ParseMode.MARKDOWN,reply_markup=InlineKeyboardMarkup(X))     
+            
+            # Check if the message is from a private chat or a group
+            if message.chat.type == "private":
+                # Personal message response
+                resp = openai.ChatCompletion.create(model=MODEL, messages=[{"role": "user", "content": a}],
+                                                    temperature=0.2)
+                x = resp['choices'][0]["message"]["content"]
+                
+                end_time = time.time()
+                telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ᴍs"
+                
+                await message.reply_text(f"{message.from_user.first_name} ᴀꜱᴋᴇᴅ:\n\n {a} \n\n {BOT_NAME} ᴀɴꜱᴡᴇʀᴇᴅ:-\n\n {x}\n\n✨ᴛɪᴍᴇ ᴛᴀᴋᴇɴ  {telegram_ping} \n\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{BOT_USERNAME} ", 
+                                         parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(X))
+            else:
+                # Group message response (customize this part based on your requirements)
+                await message.reply_text("I can only respond to private messages in this group.")
     except Exception as e:
         await message.reply_text(f"**ᴇʀʀᴏʀ: {e} ")
-
+            
 #  bard 
 
 '''bard = Bard(token=BARD_TOKEN)   
